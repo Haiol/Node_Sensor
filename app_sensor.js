95f91da454111f5d35e61812eec7501f89ddf353
Haiol <lovehp30@gmail.com>
2021-09-29 23:02:15 +0900

    CopybyHardCopyWorld

---
+++ app_sensor.js
@@ -0,0 +1,33 @@
+const express = require('express')
+const sql = require('mysql')
+const bodyParser = require('body-parser')
+const path = require('path')
+const mqtt = require('mqtt')
+var fs = require('fs')
+
+const app = express()
+var config = JSON.parse(fs.readFileSync(__dirname + '/config.json'))
+
+const sqlConn = sql.createConnection({
+  host: 'localhost',
+  user: 'root',
+  password: 'tkdwns12',
+  database: 'sensor_db',
+})
+sqlConn.connect()
+
+app.locals.pretty = true
+app.set('views', './views_file')
+app.set('view engine', 'pug')
+// app.set('views', path.join(__dirname, 'pug'));
+// app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json())
+app.use(express.static('public'))
+app.use('/script', express.static('js'))
+app.use('/node_modules', express.static(path.join(__dirname, '/node_modules')))
+
+var routeLora = require(path.join(__dirname, 'router', 'router_sensor'))(express, sqlConn, mqtt)
+app.use('/', routeLora)
+app.listen(config.server_port, () => {
+  console.log("Server has been started");
+});
